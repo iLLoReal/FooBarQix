d7f0c06b6df53b45fa397f89e9d94bb7519a04b8
olivier <olivier.laffon@epitech.eu>
2021-04-09 22:07:01 +0200

    First try of a smart math-based FooBarQix

--- FooBarQix.tsx
+++ FooBarQix.tsx
@@ -1,5 +1,29 @@
+import { string } from "yargs";
+
 const FooBarQix = (str: string): string => {
-  return ('');
+  type pairType = {
+      number: number,
+      result: string,
+    };
+  type matrixType = pairType[];
+
+  const toInt = parseInt(str);
+  const pairMatrix: matrixType = [
+    { number: 3, result: 'Foo' },
+    { number: 5, result: 'Bar' },
+    { number: 7, result: 'Qix' }
+  ];
+  let result = '';
+
+  pairMatrix.forEach((elem: pairType) => {
+    if (toInt - elem.number >= 0 && !((toInt - elem.number) % 10)) {
+      result += elem.result;
+    }
+    if (!(toInt % elem.number)) {
+      result += elem.result;
+    }
+  });
+  return result === '' ? str : result;
 };
 
 export default FooBarQix;
