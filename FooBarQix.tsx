046955a4bee31ccf8a05461cdba6e0a2ee1c0d72
olivier <olivier.laffon@epitech.eu>
2021-04-10 02:19:53 +0200

    Step2

--- FooBarQix.tsx
+++ FooBarQix.tsx
@@ -6,14 +6,17 @@ const FooBarQix = (str: string): string => {
     { number: 7, result: 'Qix' },
     { number: 5, result: 'Bar' },
     { number: 3, result: 'Foo' },
+    { number: 0, result: '*' },
   ];
   const toInt = parseInt(str);
 
   pairMatrix.forEach((elem: pairType) => {
   	str = str.replace(new RegExp(`${elem.number}`, "g"), elem.result);
-    str = (!(toInt % elem.number)) ? elem.result + str : str;
+    str = (elem.number && !(toInt % elem.number)) ? elem.result + str : str;
   });
-  str = str.replace(/[0-9]/g, '');
+  if (str.match(/[a-z]/g)) {
+    str = str.replace(/[0-9]/g, '');
+  }
   return str.length ? str : toInt.toString();
 };
 
